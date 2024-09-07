2f6b02ffff29b53064e5d1a4bb3ca7d12fec42c0
Dawid Slowik <dawidslowik379@gmail.com>
2024-09-07 13:05:42 +0200

    update folder structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import ContainerLayout from "@/layouts/ContainerLayout";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <ContainerLayout>ello</ContainerLayout>
+    </header>
+  );
+};
+
+export default Header;
